11e1fe704b87f06b90461d84ea9582e15f179d0f
Juan Felipe Torres <juanto121@hotmail.com>
2015-08-22 18:53:33 -0500

    Content formatted with SRT specification

--- public/js/srtFormatter.js
+++ public/js/srtFormatter.js
@@ -1,3 +1,4 @@
+/*SRT specification from http://www.matroska.org/technical/specs/subtitles/srt.html*/
 var SrtFormatter = (function(){
 
 	function SrtFormatter(){
@@ -7,14 +8,34 @@ var SrtFormatter = (function(){
 	var formatter = SrtFormatter.prototype;
 
 	formatter.format = function(content){
+		var output = "";
 		content.sort(function(a,b){
 			return a.time-b.time;
 		});
-		console.log(content);
+
+		//Dummy subtitle for last entry
+		content.push({time:content[content.length-1].time + 3.0,text:""});
+		
+		for(var c = 0; c < content.length-1; c++){
+			// 1 number indicating sequence
+			output += c+1 + "\n";
+			output += formatTime(content[c].time)+"-->"+formatTime(content[c+1].time)+"\n";
+			output += content[c].text+"\n\n";
+		}
+
+		console.log(output);
+		
 	};
 
 	formatter.init = function(){};
 
-	return SrtFormatter;
+	//Helpers
+	function formatTime(time){
+		var hour = (time/3600)>>0;
+		var minute = (time/60)>>0;
+		var second = time%60;
+		return hour+":"+minute+":"+second;
+	}
 
+	return SrtFormatter;
 })();
