5d26870d24b0296e2284ef4c20f325456048c98a
Juan Felipe Torres <juanto121@hotmail.com>
2015-10-21 00:52:51 -0500

    loading saved transcription

--- public/js/editor.js
+++ public/js/editor.js
@@ -3,36 +3,27 @@ var Editor = (function(){
 		this.init();
 	}
 	var editor = Editor.prototype;
+	
 	editor.init = function(){
 		this.formatter = new Formatter();
 		this.transcription = [];
+		this.loadSavedTranscription();
 	};
+
 	editor.addLine = function(keyCode){
 		if(keyCode == 13){
 			var content  = this.input.textContent;
 			if(content.trim() !== ""){
 				var seconds = this.player.getCurrentTime();
-				var min = (seconds/60)>>0;
-				var secs = (seconds%60)>>0;
-
-				this.transcription.push({
+				
+				var entry = {
 					text:content,
 					time:seconds
-				});
+				};
 
-				var prevInput = document.createElement("div");
-				var inputUser = document.createElement("div");
-				var timestamp = document.createElement("div");
+				this.transcription.push(entry);
 
-				timestamp.className = "timestamp";
-				prevInput.className = "text";
-				inputUser.className = "transcripcion";
-
-				timestamp.textContent = min + ":" + secs;
-				prevInput.textContent = content;
-
-				inputUser.appendChild(timestamp);
-				inputUser.appendChild(prevInput);
+				var inputUser = this.createTranscriptionElement(entry);
 
 				this.previn.insertBefore(inputUser,this.previn.childNodes[0]);
 				this.input.textContent = "";
@@ -47,6 +38,59 @@ var Editor = (function(){
 			}
 		}
 	};
+
+	editor.createTranscriptionElement = function(entry){
+		var content = entry.text;
+		var seconds = entry.time;
+
+		var hour = (seconds/3600) >> 0;
+		var min = (seconds-(3600*hour))/60 >> 0;
+		var secs = (seconds%60) >> 0;
+
+		var prevInput = document.createElement("div");
+		var inputUser = document.createElement("div");
+		var timestamp = document.createElement("div");
+
+		timestamp.className = "timestamp";
+		prevInput.className = "text";
+		inputUser.className = "transcripcion";
+
+		timestamp.textContent = hour + ":" + min + ":" + secs;
+		prevInput.textContent = content;
+
+		inputUser.appendChild(timestamp);
+		inputUser.appendChild(prevInput);
+
+		return inputUser;
+	};
+
+	editor.loadSavedTranscription = function(){
+		var urlPath = window.location.pathname;
+		urlPath = urlPath.substring(1,urlPath.length);
+		var parts = urlPath.split("/");
+		var transcriptionId = parts[1];
+		var that = this;
+		if(transcriptionId !== "new"){
+			$.get("/temp/files/"+transcriptionId+".srt",function(data){
+				//TODO: notify user of data retrieval.
+			})
+			.done(function(data){
+				var entries = that.formatter.deformat(data);
+				var content = document.createElement("div");
+				for(var e = 0; e < entries.length; e++){
+					var entry = entries[e];
+					that.transcription.push(entry);
+					var element =	that.createTranscriptionElement(entry);
+					content.appendChild(element);
+				}
+				that.previn.appendChild(content);
+			})
+			.fail(function(){
+				console.log("Could not find resource");
+			});
+		}
+	};
+
 	editor.setInput = function(input)
 	{
 		this.input = input;
